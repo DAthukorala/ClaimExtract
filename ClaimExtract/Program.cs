9014d456ed6ce9fede48e02527eaa8c8693ae913
DhanushkaA <dannya@gbs.kiwi>
2020-04-13 22:48:41 +1200

    Add project files.

---
+++ ClaimExtract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ClaimExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileText = File.ReadAllText("ClaimData.txt");
+            var claimPattern = @"(?<=~CLM\*)([0-9]*)(?=\*)";
+            var claimNumbers = Regex.Matches(fileText, claimPattern);
+            foreach (var id in claimNumbers)
+            {
+                Console.Write($"{id}, ");
+            }
+
+            var refPattern = @"\~REF\*\*\~";
+            var refCount = Regex.Matches(fileText, refPattern).Count;
+            Console.WriteLine($"Ref count:{refCount}");
+            Console.Read();
+        }
+    }
+}
